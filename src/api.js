0edb62aa83c1aa452366fbc8978cd1deb112d16e
Emil Ahlbäck <Emil.Ahlback99@gmail.com>
2018-04-17 17:01:01 +0200

    auth no

--- src/api.js
+++ src/api.js
@@ -30,7 +30,7 @@ export function authorize(email, password) {
 		}).then(response => {
 			res(response.data);
 		}).catch(err => {
-			console.log(err)
+			console.error(err);
 			rej(err);
 		});
 	})
